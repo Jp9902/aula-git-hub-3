a1b28a086924bb484f62ede6c968d241185d9369
João <168004754+JnGuilherme07@users.noreply.github.com>
2024-09-18 13:03:20 -0300

    Máquina de gerar futuros aleatoriamente

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-const name = ['Joao', 'Caio','Julia', 'Guilherme']
-console.log(nome[1])
+const Future = ['Você ficará rico', 'Você Morrerá logo','Você vai conhecer sua alma gêmea', 'Você vai fazer algo incrivel']
 
 function gerarAleatorio(){
-    const numeroAleatorio = Math.floor(Math.random() * 4)
+    const numeroAleatorio = Math.floor(Math.random() * Future.length)
     return numeroAleatorio
 }
 
-function sortearNome(){
-    let numeroSorteado = gerarAleatorio()
-    console.log(nomes(numeroSorteado))
-    
+function futuro(){
+    let FuturoSorteado = gerarAleatorio()
+    window.alert(Future[FuturoSorteado])
+
 
 }
